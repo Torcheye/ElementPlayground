89a80998bc721d6d99e939689df10ef5d74950f8
King Zhou <zhouyimingking@outlook.com>
2023-06-29 14:53:10 -0700

    temp-not working

---
+++ Assets/Scripts/CellUpdateSystem.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Rendering;
+using UnityEngine;
+
+[BurstCompile]
+[UpdateInGroup(typeof(VariableRateSimulationSystemGroup))]
+public partial struct CellUpdateSystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<Spawner>();
+    }
+    
+    
+    public void OnUpdate(ref SystemState state)
+    {
+        var eBuffer = SystemAPI.GetSingletonBuffer<CellBuffer>().Reinterpret<Entity>();
+        var spawner = SystemAPI.GetSingleton<Spawner>();
+
+        new CellUpdateJob()
+        {
+            eBuffer = eBuffer,
+            entityManager = state.EntityManager,
+            spawner = spawner
+        }.Run();
+
+        new CellResetJob().ScheduleParallel();
+    }
+}
+
+partial struct CellUpdateJob : IJobEntity
+{
+    public DynamicBuffer<Entity> eBuffer;
+    public EntityManager entityManager;
+    public Spawner spawner;
+
+    [BurstCompile]
+    void Execute(ref Cell c)
+    {
+        int id = c.Id;
+        if (c.Updated)
+            return;
+        if (c.Type == 0)
+            return;
+
+        int n = GetNeighbor(id, 1, 0);
+
+        if (n == -1)
+            return;
+        if (entityManager.GetComponentData<Cell>(eBuffer[n]).Updated)
+            return;
+
+        // update neighbor
+        entityManager.SetComponentData(eBuffer[n], new Cell()
+        {
+            Id = n,
+            Type = 1,
+            Updated = true
+        });
+        entityManager.SetComponentData(eBuffer[n], new URPMaterialPropertyBaseColor()
+        {
+            Value = new float4(0, 1, 1, 1)
+        });
+
+        // update self
+        c.Type = 0;
+        c.Updated = true;
+        entityManager.SetComponentData(eBuffer[id], new URPMaterialPropertyBaseColor()
+        {
+            Value = float4.zero
+        });
+    }
+    
+    [BurstCompile]
+    private int GetNeighbor(in int id, in int x, in int y)
+    {
+        int row = id / spawner.CountX;
+        int col = id % spawner.CountX;
+        
+        if (row == 0 && y < 0 || row == spawner.CountY - 1 && y > 0 ||
+            col == 0 && x < 0 || col == spawner.CountX - 1 && x > 0)
+        {
+            return -1;
+        }
+
+        return id + y * spawner.CountX + x;
+    }
+}
+
+/// <summary>
+/// Reset cells for next update
+/// </summary>
+[BurstCompile]
+partial struct CellResetJob : IJobEntity
+{
+    void Execute(ref Cell c)
+    {
+        if (!c.Updated)
+            return;
+        c.Updated = false;
+    }
+}
--- Assets/Scripts/SpawnerSystem.cs
+++ Assets/Scripts/SpawnerSystem.cs
@@ -9,12 +9,8 @@ using Unity.Rendering;
 using UnityEngine;
 
 [BurstCompile]
-[UpdateInGroup(typeof(VariableRateSimulationSystemGroup))]
 public partial struct SpawnerSystem : ISystem, ISystemStartStop
 {
-    private int _countX, _countY;
-    public Entity bufferEntity;
-    
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<UpdateRate>();
@@ -27,19 +23,17 @@ public partial struct SpawnerSystem : ISystem, ISystemStartStop
         state.World.GetExistingSystemManaged<VariableRateSimulationSystemGroup>().RateManager = new RateUtils.VariableRateManager(SystemAPI.GetSingleton<UpdateRate>().MilliSeconds);
         
         var entityManager = state.EntityManager;
-        bufferEntity = entityManager.CreateEntity();
+        var bufferEntity = entityManager.CreateEntity();
         entityManager.AddBuffer<CellBuffer>(bufferEntity);
         
         Spawner spawner = SystemAPI.GetSingleton<Spawner>();
-        _countX = spawner.CountX;
-        _countY = spawner.CountY;
         
         int id = 0;
-        float startX = -(_countX - 1) * 0.5f * spawner.Distance;
-        float startY = -(_countY - 1) * 0.5f * spawner.Distance;
-        for (float y = 0; y < _countY; y ++)
+        float startX = -(spawner.CountX - 1) * 0.5f * spawner.Distance;
+        float startY = -(spawner.CountY - 1) * 0.5f * spawner.Distance;
+        for (float y = 0; y < spawner.CountY; y ++)
         {
-            for (float x = 0; x <_countX; x ++)
+            for (float x = 0; x <spawner.CountX; x ++)
             {
                 Cell cellComponent = new Cell
                 {
@@ -66,71 +60,8 @@ public partial struct SpawnerSystem : ISystem, ISystemStartStop
         }
     }
 
-    [BurstCompile]
-    private int GetNeighbor(in int id, in int x, in int y)
-    {
-        int row = id / _countX;
-        int col = id % _countX;
-        
-        if (row == 0 && y < 0 || row == _countY - 1 && y > 0 ||
-            col == 0 && x < 0 || col == _countX - 1 && x > 0)
-        {
-            return -1;
-        }
-
-        return id + y * _countX + x;
-    }
-
-    [BurstCompile]
-    public void OnUpdate(ref SystemState state)
-    {
-        var buffer = SystemAPI.GetBuffer<CellBuffer>(bufferEntity);
-        var eBuffer = buffer.Reinterpret<Entity>();
-
-        foreach (RefRW<Cell> c in SystemAPI.Query<RefRW<Cell>>())
-        {
-            int id = c.ValueRO.Id;
-            if (c.ValueRO.Updated)
-                continue;
-            if (c.ValueRO.Type == 0) 
-                continue;
-            
-            int n = GetNeighbor(id, 1, 0);
-
-            if (n == -1) 
-                continue;
-            if (SystemAPI.GetComponentRO<Cell>(eBuffer[n]).ValueRO.Updated)
-                continue;
-                    
-            // set neighbor
-            SystemAPI.SetComponent(eBuffer[n], new Cell()
-            {
-                Id = n,
-                Type = 1,
-                Updated = true
-            });
-            SystemAPI.SetComponent(eBuffer[n], new URPMaterialPropertyBaseColor()
-            {
-                Value = new float4(0, 1, 1, 1)
-            });
-                    
-            // set self
-            c.ValueRW.Type = 0;
-            c.ValueRW.Updated = true;
-            SystemAPI.SetComponent(eBuffer[id], new URPMaterialPropertyBaseColor()
-            {
-                Value = float4.zero
-            });
-        }
-
-        // reset
-        foreach (RefRW<Cell> c in SystemAPI.Query<RefRW<Cell>>())
-        {
-            if (!c.ValueRO.Updated)
-                continue;
-            c.ValueRW.Updated = false;
-        }
-    }
-
     public void OnStopRunning(ref SystemState state) {}
 }
+
+
+
